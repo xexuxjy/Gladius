3200ec4ea10fab08be97f1c760b7bd97d59adcf8
xexuxjy <markneale@hotmail.com>
2021-08-02 15:45:54 +0100

    working as unpack / pack

---
+++ tools/repack-gc.bat
@@ -0,0 +1,10 @@
+@echo off
+set BASE_DIR=D:\GladiusISOWorkingExtracted\python-gc\
+set OUTPUT_DIR=%BASE_DIR%\gc\data\config\
+set EXTRACTED_DIR=%BASE_DIR%\gc\
+set BEC_FILE=%BASE_DIR%\repack\gladius.bec
+set ISO_FOLDER=%BASE_DIR%\repack\
+set ISO_OUTPUT_FILE=%BASE_DIR%\GladiusMODDED.iso
+
+py -3 bec-tool-gc.py -pack %EXTRACTED_DIR% %BEC_FILE% %EXTRACTED_DIR%/FileList.txt
+py -3 ngciso-tool.py -pack %ISO_FOLDER% %ISO_FOLDER%fst.bin %ISO_FOLDER%repack_FileList.txt %ISO_OUTPUT_FILE%
