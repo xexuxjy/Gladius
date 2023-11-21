a0e22833a2dbd8cf93054ba2e065f23baf378fc9
xexuxjy <markneale@hotmail.com>
2023-11-21 15:13:06 +0000

    restored gc packing

--- tools/repack-gc.bat
+++ tools/repack-gc.bat
@@ -1,15 +1,15 @@
 rem @echo off
-set BASE_DIR=F:\GladiusGCPacking
+set BASE_DIR=D:\GladiusModding\GC\
 set OUTPUT_DIR=%BASE_DIR%\gc\data\config\
-set BEC_UNPACK_DIR=%BASE_DIR%\BEC\unpack\
+set BEC_UNPACK_DIR=%BASE_DIR%\UNPACK
 
-set BEC_FILE=%BASE_DIR%\BEC\repack\gladius.bec
-set ISO_FOLDER=%BASE_DIR%\ISO\repack\
+set BEC_FILE=%BASE_DIR%\REPACK\gladius.bec
+set ISO_FOLDER=%BASE_DIR%\ISO\unpack\
 set ISO_OUTPUT_FILE=%BASE_DIR%\ISO\GladiusMODDED.iso
 
 py -3 bec-tool-gc.py -pack %BEC_UNPACK_DIR% %BEC_FILE% %BEC_UNPACK_DIR%/FileList.txt
-copy %BEC_FILE% %ISO_FOLDER%
-py -3 ngciso-tool.py -pack %ISO_FOLDER% %ISO_FOLDER%fst.bin %ISO_FOLDER%GladiusISO_FileList.txt %ISO_OUTPUT_FILE%
+rem copy %BEC_FILE% %ISO_FOLDER%
+rem py -3 ngciso-tool.py -pack %ISO_FOLDER% %ISO_FOLDER%fst.bin %ISO_FOLDER%GladiusISO_FileList.txt %ISO_OUTPUT_FILE%
 
 
 
--- tools/unpack-gc.bat
+++ tools/unpack-gc.bat
@@ -1,12 +1,12 @@
 @echo off
-set BASE_DIR=F:\GladiusGCPacking
-set ORIGINAL_ISO_FILE=%BASE_DIR%\ISO\Gladius.nkit.iso
+set BASE_DIR=D:\GladiusModding\GC\
+set ORIGINAL_ISO_FILE=%BASE_DIR%\ISO\Gladius-GameCube.iso
 set ISO_UNPACK_DIR=%BASE_DIR%\ISO\unpack\
 set BEC_FILE=%ISO_UNPACK_DIR%\gladius.bec
-set BEC_UNPACK_DIR=%BASE_DIR%\BEC\unpack\
+set BEC_UNPACK_DIR=%BASE_DIR%\unpack\
 
-rem py -3 ngciso-tool-gc.py -unpack %ORIGINAL_ISO_FILE% %ISO_UNPACK_DIR% GladiusISO_FileList.txt
-py -3 bec-tool-gc.py -unpack %BEC_FILE% %BEC_UNPACK_DIR% 
+py -3 ngciso-tool-gc.py -unpack %ORIGINAL_ISO_FILE% %ISO_UNPACK_DIR% GladiusISO_FileList.txt
+py -3 bec-tool-all.py -unpack %BEC_FILE% %BEC_UNPACK_DIR% 
 
 
 
